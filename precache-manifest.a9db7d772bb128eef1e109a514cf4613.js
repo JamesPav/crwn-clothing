ae328a50741f1619467b189dd81b12c72ba506b8
Jim Pav <jamesvlid@hotmail.com>
2020-06-11 04:48:07 +0300

    Updates

---
+++ precache-manifest.a9db7d772bb128eef1e109a514cf4613.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81970bbbc2664e620ae70fa715a97276",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "59c0e8368284e2361be7",
+    "url": "/crwn-clothing/static/css/main.8703559d.chunk.css"
+  },
+  {
+    "revision": "dd875ce6b04f5657afcd",
+    "url": "/crwn-clothing/static/js/2.2a5e5e95.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.2a5e5e95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c0e8368284e2361be7",
+    "url": "/crwn-clothing/static/js/main.a124a8d6.chunk.js"
+  },
+  {
+    "revision": "8558d1d6d2663c53b558",
+    "url": "/crwn-clothing/static/js/runtime-main.742f8efe.js"
+  }
+]);
